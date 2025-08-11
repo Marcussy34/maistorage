8f829715174ead7e43688923c8897619420e15e0
Marcus Tan <marcus.tanchiyau@gmail.com>
2025-08-11 20:50:53 +0800

    Updated font styling

--- apps/web/pages/_app.js
+++ apps/web/pages/_app.js
@@ -1,15 +1,29 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import { StrictMode } from 'react';
+import { Baloo_2 } from 'next/font/google';
+
+// Configure Baloo 2 font with optimal settings
+const baloo2 = Baloo_2({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-baloo2',
+});
 
 export default function App({ Component, pageProps }) {
   // Disable StrictMode in development to prevent double execution
   if (process.env.NODE_ENV === 'development') {
-    return <Component {...pageProps} />;
+    return (
+      <main className={`${baloo2.className} ${baloo2.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    );
   }
   
   return (
     <StrictMode>
-      <Component {...pageProps} />
+      <main className={`${baloo2.className} ${baloo2.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </StrictMode>
   );
 }
